5bcfe071691e83d8f39a545fa2862043a6c80b70
Morgan.lee <morgan.lee@marqvision.com>
2025-08-02 17:59:58 +0900

    style: 레이아웃 디자인 변경

--- src/app/_component/layout/Header/index.tsx
+++ src/app/_component/layout/Header/index.tsx
@@ -1,26 +1,40 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import List from '@/app/_asset/icon/list.svg';
 import Logo from '@/app/_asset/icon/logo.svg';
-import User from '@/app/_asset/icon/user.svg';
 import { Button } from '@/app/_component/common/Button';
 import styles from './index.module.css';
 
 export const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setIsScrolled(scrollTop > 50); // 50px 이상 스크롤했을 때 상태 변경
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isScrolled ? styles.scrolled : ''}`}>
       <h1 className={styles.logo}>
-        <Logo alt="logo" width={36} height={36} />
-        <p>청약닷컴</p>
+        <Button variant="tertiary" href="/">
+          <Logo alt="logo" width={20} height={20} />
+          <p>청약닷컴</p>
+        </Button>
       </h1>
       <nav className={styles['navigation']}>
         <ul className={styles['navigation__list']}>
           <li>
             <Button variant="tertiary" href="/">
-              <List width={24} height={24} alt="List" />
-            </Button>
-          </li>
-          <li>
-            <Button variant="tertiary" href="/">
-              <User width={24} height={24} alt="User" />
+              <List width={20} height={20} alt="List" />
             </Button>
           </li>
         </ul>
